7ef8384d4d576b5a34326188bd4e3cf842b67a2c
WilliamTouboul <will.touboul@gmail.com>
2021-04-25 18:31:26 +0200

    + de tomes

--- views/testOP2.php
+++ views/testOP2.php
@@ -1,7 +1,7 @@
 <?php include('header.php');
 
 try {
-    $queryOPAllCover = 'SELECT * FROM volume WHERE ID_Manga = 9 ORDER BY releaseDate DESC';
+    $queryOPAllCover = 'SELECT * FROM volume WHERE ID_Manga = 17 ORDER BY releaseDate DESC';
     $getCovers = $pdo->query($queryOPAllCover);
     $covers = $getCovers->fetchAll(PDO::FETCH_ASSOC);
     // On verifie si le fetch nous renvoi quelque chose
@@ -9,7 +9,7 @@ try {
         // On ecrit chaque truc qu'on veut depuis le fetch
         foreach ($covers as $covers) {
 ?>
-            <img style="width : 33%" src="<?= $covers['cover'] ?>">
+            <img style="width : 26%;margin : 5px;" src="<?= $covers['cover'] ?>">
 
 <?php
         }
@@ -17,4 +17,6 @@ try {
 } catch (\PDOException $e) {
     die($e->getMessage());
 }
+
+include('footer.php');
 ?>
