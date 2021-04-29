120be1a3e40a75a3ebe6c0e603e8f0bc805403d3
WilliamTouboul <will.touboul@gmail.com>
2021-04-29 17:27:02 +0200

    pushpush

--- controllers/cardMangaEditorController.php
+++ controllers/cardMangaEditorController.php
@@ -5,12 +5,12 @@ try {
     $queryMangaForeachEditor = 'SELECT manga.ID as mangaID, manga.Name AS manganame, manga.Author, manga.releaseDate, manga.Synopsis, manga.photo AS mangaphoto, editor.Name, editor.link from manga LEFT JOIN editor ON ID_Editor = editor.ID WHERE editor.ID =' . $idToFetch;
     $getMangas = $pdo->query($queryMangaForeachEditor);
     $mangas = $getMangas->fetchAll(PDO::FETCH_ASSOC);
+    
     // On verifie si le fetch nous renvoi quelque chose
     if ($mangas) {
         // On ecrit chaque truc qu'on veut depuis le fetch
         foreach ($mangas as $mangas) {
 ?>
-
             <div class="card cardMangaEditor">
                 <a href="viewManga.php?<?= $mangas['mangaID'] ?>">
 
@@ -26,7 +26,9 @@ try {
 
 <?php
         }
+
     }
+
 } catch (\PDOException $e) {
     die($e->getMessage());
 }
